02c1a703c5f66482cc350282071a2445f06bd522
Elder Oliveira <elderoliveiracastro@gmail.com>
2024-10-08 20:03:48 -0300

    Code challange 3

--- script.js
+++ script.js
@@ -151,3 +151,58 @@ for (const [index, player] of game.scored.entries()) {
 }
 
 console.log(scorers);
+
+/*
+
+    Let's continue with our football betting app! This time, we have a map called 
+    'gameEvents' (see below) with a log of the events that happened during the 
+    game. The values are the events themselves, and the keys are the minutes in which 
+    each event happened (a football game has 90 minutes plus some extra time).
+    Your tasks:
+    1. Create an array 'events' of the different game events that happened (no 
+    duplicates)
+    2. After the game has finished, is was found that the yellow card from minute 64 
+    was unfair. So remove this event from the game events log.
+    3. Compute and log the following string to the console: "An event happened, on 
+    average, every 9 minutes" (keep in mind that a game has 90 minutes)
+    4. Loop over 'gameEvents' and log each element to the console, marking 
+    whether it's in the first half or second half (after 45 min) of the game, like this:
+    [FIRST HALF] 17: ⚽ GOAL
+
+*/
+
+const gameEvents = new Map([
+  [17, "GOAL"],
+  [36, "Substitution"],
+  [47, "GOAL"],
+  [61, "Substitution"],
+  [64, "Yellow card"],
+  [69, "Red card"],
+  [70, "Substitution"],
+  [72, "Substitution"],
+  [76, "GOAL"],
+  [80, "GOAL"],
+  [92, "Yellow card"],
+]);
+
+//1
+const mapEvents = new Set();
+for (const [minute, event] of gameEvents) mapEvents.add(event);
+
+const arrEventsNoDuplicate = [...mapEvents];
+
+console.log(arrEventsNoDuplicate);
+
+//2
+gameEvents.delete(64);
+console.log(gameEvents);
+
+//3 ??
+const totalEvents = gameEvents.size;
+let totalMinutes = 0;
+for (const [minute, event] of gameEvents) totalMinutes += minute;
+console.log(
+  `An event happened, on average, every ${totalMinutes / totalEvents} minutes`
+);
+
+//4 I've got to finish this one.
